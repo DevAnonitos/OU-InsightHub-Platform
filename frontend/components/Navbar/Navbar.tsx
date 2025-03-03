948a5a42d23a7e77c7ff519509888f8739d9dbac
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2025-03-03 17:19:33 +0700

    Frontend: Install Next.js Ver 14 & ConfigApp

---
+++ frontend/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SidebarTrigger } from '../ui/sidebar';
+
+const Navbar = () => {
+  return (
+    <div className='flex w-full border-b-[1px] h-12'>
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
